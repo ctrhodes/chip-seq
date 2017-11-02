42e2ee25d77f3d69cad642bdc3d724b2a6042766
Christopher Rhodes <ctrhodes@users.noreply.github.com>
2017-11-02 06:13:58 -0500

    Add files via upload

---
+++ step1_bowtie_align_chip_reads.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IN_DIR = "/home/chris/project/chip/s1_fastq"
+OUT_DIR = "/home/chris/project/chip/s2_aligned"
+
+cd $IN_DIR
+
+genome="PapAnu2NoM_Ensb"
+
+#select file
+for sample in *fastq
+do
+describer=$(echo ${sample} | sed 's/.fastq//')_${genome}
+echo $sample
+echo $describer
+echo $genome
+
+#select analysis
+bowtie --mm -p 8 -S -m 1 --best --phred33-quals $genome $sample > $OUT_DIR/${describer}.sam 2> $OUT_DIR/${describer}_alignment_stats.txt
+echo $describer endBowtie
+
+done
+
